995931280b038cbe100754017711c75746113008
Eldad Zack <eldad@fogrefinery.com>
2023-02-04 13:17:40 +0100

    output info as yaml

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use gltf::Gltf;
+use serde::Serialize;
 
 /// gltf tool
 ///
@@ -22,11 +23,26 @@ enum Action {
     Info,
 }
 
-fn show_info(gltf: Gltf) {
-    println!("Materials:");
-    gltf.materials()
-        .flat_map(|t| t.name().map(str::to_owned))
-        .for_each(|name| println!("- {name}"));
+#[derive(Serialize, Debug)]
+struct GltfInfo {
+    material_names: Vec<String>,
+}
+
+impl From<Gltf> for GltfInfo {
+    fn from(gltf: Gltf) -> Self {
+        let material_names: Vec<String> = gltf.materials().flat_map(|t| t.name().map(str::to_owned)).collect();
+
+        Self {
+            material_names
+        }
+    }
+}
+
+fn show_info(gltf: Gltf) -> anyhow::Result<()> {
+    let info: GltfInfo = gltf.into();
+    let yaml = serde_yaml::to_string(&info)?;
+    println!("{yaml}");
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
@@ -35,7 +51,7 @@ fn main() -> anyhow::Result<()> {
     let gltf = Gltf::open(args.gltf_filename)?;
 
     match args.action {
-        Action::Info => show_info(gltf),
+        Action::Info => show_info(gltf)?,
     }
 
     Ok(())
